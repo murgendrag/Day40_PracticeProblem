44e32cff6824695b8e79ad852fa8aa8990032ceb
murgendrag <murgendra67@gmail.com>
2022-04-24 17:18:40 +0530

    Power of 2 table

---
+++ PowerofTwoTable.js
@@ -0,0 +1,15 @@
+Console.log("Table of Power of 2");
+
+let n = prompt("Enter the value of N for Table);
+
+if (n!=0) 
+{
+    for (var i =0;i<n;i++)
+    {
+       power=2*power;
+    }  
+}
+else
+{
+    console.log("Value is 1");
+}
